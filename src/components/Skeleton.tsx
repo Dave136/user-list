3a82cef4ac4fa9806a1a41543a1ad430f9f5db8d
Dave136 <davejs136@gmail.com>
2022-11-23 17:41:33 -0400

    refactor: skeleton loader size

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,64 +1,64 @@
 const Skeleton = () => {
   return (
     <div className="animate-pulse w-full">
-      <div className="w-full bg-light-300 h-lg rounded-md">
-        <div className="w-full bg-zinc-400 h-16 flex items-center gap-8 pl-6 rounded-t-md">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+      <div className="w-full bg-light-300 h-[382px] rounded-md">
+        <div className="w-full bg-zinc-200 h-12 flex items-center gap-8 pl-6 rounded-t-md">
+          <div className="w-28 bg-zinc-100 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-100 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-100 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-100 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
-        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6 rounded-b-md">
-          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-12 flex items-center gap-8 pl-6 rounded-b-md">
+          <div className="w-28 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-24 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-32 bg-zinc-200 h-4 rounded-md"></div>
+          <div className="w-14 bg-zinc-200 h-4 rounded-md"></div>
         </div>
       </div>
-      <div className="w-full mt-8 bg-light-300">
+      <div className="w-full mt-4">
         <div className="w-full h-16 flex items-center gap-4 justify-center">
-          <div className="w-16 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-8 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-8 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-8 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-8 bg-zinc-300 h-6 rounded-md"></div>
-          <div className="w-16 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-16 bg-zinc-200 h-8 rounded-md"></div>
+          <div className="w-8 bg-zinc-200 h-8 rounded-md"></div>
+          <div className="w-8 bg-zinc-200 h-8 rounded-md"></div>
+          <div className="w-8 bg-zinc-200 h-8 rounded-md"></div>
+          <div className="w-8 bg-zinc-200 h-8 rounded-md"></div>
+          <div className="w-16 bg-zinc-200 h-8 rounded-md"></div>
         </div>
       </div>
     </div>
