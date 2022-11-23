6038575a7fe96e97dfb2df4e7a0421c24d6b34d6
Dave136 <davejs136@gmail.com>
2022-11-23 16:05:57 -0400

    add: skeleton loading

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,58 @@
+const Skeleton = () => {
+  return (
+    <div className="animate-pulse w-full">
+      <div className="w-full bg-light-300 h-lg rounded-md">
+        <div className="w-full bg-zinc-400 h-16 flex items-center gap-8 pl-6 rounded-t-md">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+        <div className="w-full border border-zinc-300 border-t-transparent h-16 flex items-center gap-8 pl-6 rounded-b-md">
+          <div className="w-28 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-24 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-32 bg-zinc-300 h-6 rounded-md"></div>
+          <div className="w-14 bg-zinc-300 h-6 rounded-md"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skeleton;
